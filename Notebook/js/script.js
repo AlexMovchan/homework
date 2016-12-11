256c5653254c6d11e04509d15e50d1e7252c3223
Alex <alexangel_@mail.ru>
2016-12-11 12:05:21 +0200

    h/w

--- Notebook/js/script.js
+++ Notebook/js/script.js
@@ -14,15 +14,21 @@ window.onload= function() {
             second:'i5'
         };
         this.touchpad = true;
-        function touckpadOff(){
-            this.touchpad = false;
-        } 
+       
     }
  
+    Notebook.prototype.touckpadOff = function(){
+        this.touchpad = false;
+    }
+    
     
     var Acer = new Notebook('<img src="img/acer.png" width="300px" height="200px">', 'Acer', 'Aspire One');
     var Asus = new Notebook('<img src="img/asus.png" width="300px" height="200px">', 'Asus', 'x540la');
     var HP = new Notebook('<img src="img/hp.png" width="300px" height="200px">', 'HP', 'Pavillion');
+    var Lenovo = new Notebook('<img src="img/lenovo.png" width="300px" height="200px">', 'Lenovo', 'HZ');
+    var Mac = new Notebook('<img src="img/mac.png" width="300px" height="200px">', 'Mac', 'Apple');
+    var Idea = new Notebook('<img src="img/idea.png" width="300px" height="200px">', 'Idea', 'HZ');
+    
     
     function showNotebook(){
         document.write('<table width="700px" height="500px" border="1px" cellspacing="0" cellpadding="0" align="center"><tbody>')
@@ -45,6 +51,8 @@ window.onload= function() {
                     document.write('<option>' + Acer.CPU[c] + '</option>')
                 }
                 document.write('</select></td></tr>')
+            }else if(p == 'touckpadOff'){
+                document.write('<tr><td>' + p + '</td><td>You can change this property</td></tr>');
             }else{
                 document.write('<tr><td width="200px">' + p + '</td><td>' + Acer[p] + '</td></tr>')
             }
@@ -73,6 +81,8 @@ window.onload= function() {
                     document.write('<option>' + Asus.CPU[c] + '</option>')
                 }
                 document.write('</select></td></tr>')
+            }else if(p == 'touckpadOff'){
+                document.write('<tr><td>' + p + '</td><td>You can change this property</td></tr>');
             }else{
                 document.write('<tr><td width="200px">' + p + '</td><td>' + Asus[p] + '</td></tr>')
             }
@@ -101,13 +111,108 @@ window.onload= function() {
                     document.write('<option>' + HP.CPU[c] + '</option>')
                 }
                 document.write('</select></td></tr>')
+            }else if(p == 'touckpadOff'){
+                document.write('<tr><td>' + p + '</td><td>You can change this property</td></tr>');
             }else{
                 document.write('<tr><td width="200px">' + p + '</td><td>' + HP[p] + '</td></tr>')
             }
         }
         document.write('</tbody></table>')
+       
+        document.write('<br>')
+        
+        document.write('<table width="700px" height="500px" border="1px" cellspacing="0" cellpadding="0" align="center"><tbody>')
+        for(var p in Lenovo){
+            if(p == 'icon'){
+                document.write('<tr><td colspan="2" align="center">' + Lenovo[p] + '</td></tr>');
+            }else if(p == 'touchpad' && Lenovo['touchpad'] == true){
+                document.write('<tr><td>' + p + '</td><td>YES</td></tr>');
+            }else if(p == 'touchpad' && Lenovo['touchpad'] == false){
+                document.write('<tr><td>' + p + '</td><td>NO</td></tr>');
+            }else if(p == 'RAM'){
+                document.write('<tr><td>RAM</td><td><select>')
+                for(r in Lenovo.RAM){
+                    document.write('<option>' + Lenovo.RAM[r] + '</option>')
+                }
+                document.write('</select></td></tr>')
+            }else if(p == 'CPU'){
+                document.write('<tr><td>CPU</td><td><select>')
+                for(c in Lenovo.CPU){
+                    document.write('<option>' + Lenovo.CPU[c] + '</option>')
+                }
+                document.write('</select></td></tr>')
+            }else if(p == 'touckpadOff'){
+                document.write('<tr><td>' + p + '</td><td>You can change this property</td></tr>');
+            }else{
+                document.write('<tr><td width="200px">' + p + '</td><td>' + Lenovo[p] + '</td></tr>')
+            }
+        }
+        document.write('</tbody></table>')
+        
+        document.write('<br>')
+        
+        document.write('<table width="700px" height="500px" border="1px" cellspacing="0" cellpadding="0" align="center"><tbody>')
+        for(var p in Mac){
+            if(p == 'icon'){
+                document.write('<tr><td colspan="2" align="center">' + Mac[p] + '</td></tr>');
+            }else if(p == 'touchpad' && Mac['touchpad'] == true){
+                document.write('<tr><td>' + p + '</td><td>YES</td></tr>');
+            }else if(p == 'touchpad' && Mac['touchpad'] == false){
+                document.write('<tr><td>' + p + '</td><td>NO</td></tr>');
+            }else if(p == 'RAM'){
+                document.write('<tr><td>RAM</td><td><select>')
+                for(r in Mac.RAM){
+                    document.write('<option>' + Mac.RAM[r] + '</option>')
+                }
+                document.write('</select></td></tr>')
+            }else if(p == 'CPU'){
+                document.write('<tr><td>CPU</td><td><select>')
+                for(c in Mac.CPU){
+                    document.write('<option>' + Mac.CPU[c] + '</option>')
+                }
+                document.write('</select></td></tr>')
+            }else if(p == 'touckpadOff'){
+                document.write('<tr><td>' + p + '</td><td>You can change this property</td></tr>');
+            }else{
+                document.write('<tr><td width="200px">' + p + '</td><td>' + Mac[p] + '</td></tr>')
+            }
+        }
+        document.write('</tbody></table>')
+        
+        document.write('<br>')
+        
+        document.write('<table width="700px" height="500px" border="1px" cellspacing="0" cellpadding="0" align="center"><tbody>')
+        
+        for(var p in Idea){
+            if(p == 'icon'){
+                document.write('<tr><td colspan="2" align="center">' + Idea[p] + '</td></tr>');
+            }else if(p == 'touchpad' && Idea['touchpad'] == true){
+                document.write('<tr><td>' + p + '</td><td>YES</td></tr>');
+            }else if(p == 'touchpad' && Idea['touchpad'] == false){
+                document.write('<tr><td>' + p + '</td><td>NO</td></tr>');
+            }else if(p == 'RAM'){
+                document.write('<tr><td>RAM</td><td><select>')
+                for(r in Idea.RAM){
+                    document.write('<option>' + Idea.RAM[r] + '</option>')
+                }
+                document.write('</select></td></tr>')
+            }else if(p == 'CPU'){
+                document.write('<tr><td>CPU</td><td><select>')
+                for(c in Idea.CPU){
+                    document.write('<option>' + Idea.CPU[c] + '</option>')
+                }
+                document.write('</select></td></tr>')
+            }else if(p == 'touckpadOff'){
+                document.write('<tr><td>' + p + '</td><td>You can change this property</td></tr>');
+            }else{
+                document.write('<tr><td width="200px">' + p + '</td><td>' + Idea[p] + '</td></tr>')
+            }
+        }
+        document.write('</tbody></table>')
+
     }
     
+    Acer.touckpadOff();
    showNotebook();
     
     
--- Phone/js/script.js
+++ Phone/js/script.js
@@ -1,36 +1,50 @@
 //window.onload= function() {
         
-  function Phone(model, color, RAM, camera, photoof) {
-      this.model = model,
-      this.color = color,
-      this.RAM = RAM,
-      this.camera = true,
+  function Phone(icon, model, color, RAM, camera, photoof) {
+      this.icon = icon;
+      this.model = model;
+      this.color = color;
+      this.RAM = RAM;
+      this.camera = true;
       this.photoof = function(){
           this.camera = false;
-      },
+      };
       this.ring = function(){
           alert('ring_ring_ring');
       }
   } 
- function print(Phone){
-        for(var n in Phone){
-            document.write(n + ': ' + Phone[n] + '<br>')
+
+  
+
+var Nokia = new Phone('<img src="img/nokia.jpg" width="80px" height="100px">', '3310', 'white', '1Gb');
+var Apple = new Phone('<img src="img/apple.jpg" width="80px" height="100px">', 'iphone5', 'black', '2Gb', true);
+var Samsung = new Phone('<img src="img/samsung.png" width="80px" height="100px">', 'galaxyS', 'white', '2Gb', true);
+
+
+function showPhone(){
+    document.write('<table width="700px" height="500px" border>')
+    for(var p in Nokia){
+        if(p == 'ring'){
+            document.write('<td>Yes</td>')
+        }else if(p == 'camera'  && Nokia['camera'] == true){
+            document.write('<td>Yes</td>)
         }
     }
-    
-    
-
-var Nokia = new Phone('3310', 'white', '1Gb', true);
-var Apple = new Phone('iphone5', 'black', '2Gb', true);
-var Samsung = new Phone('galaxyS', 'white', '2Gb', true);
+}
 
 
 
+
+
+
+
+
+
+
     
-    
-document.getElementById('first').innerHTML = '<img src="img/nokia.jpg" width="80px" height="100px"><br>' + Nokia.model + '<br>' + Nokia.color + '<br>' + Nokia.RAM + '<br>' + Nokia.camera;
+/*document.getElementById('first').innerHTML = '<img src="img/nokia.jpg" width="80px" height="100px"><br>' + Nokia.model + '<br>' + Nokia.color + '<br>' + Nokia.RAM + '<br>' + Nokia.camera;
 document.getElementById('second').innerHTML = '<img src="img/apple.jpg" width="80px" height="100px"><br>' + Apple.model + '<br>' + Apple.color + '<br>' + Apple.RAM + '<br>' + Apple.camera;
-document.getElementById('third').innerHTML = '<img src="img/samsung.png" width="80px" height="100px"><br>' + Samsung.model + '<br>' + Samsung.color + '<br>' + Samsung.RAM + '<br>' + Samsung.camera;
+document.getElementById('third').innerHTML = '<img src="img/samsung.png" width="80px" height="100px"><br>' + Samsung.model + '<br>' + Samsung.color + '<br>' + Samsung.RAM + '<br>' + Samsung.camera;*/
     
 
     
